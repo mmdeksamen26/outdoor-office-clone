70bc827cf3133e46732ebf65a5b92a3d11dd1150
oliverjrobles <oliver.robles@live.dk>
2026-05-21 16:51:23 +0200

    ny hero video

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <section>
       <div className="relative w-full overflow-hidden">
         <video autoPlay muted loop playsInline preload="none" className="h-[78vh] w-full object-cover">
-          <source src="/video/OOHeroVideo1.mp4" type="video/mp4" />
+          <source src="/video/finalHeroVideo.mp4" type="video/mp4" />
         </video>
       </div>
     </section>
